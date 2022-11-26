31646adb7b091ae63eb69b296ecf4d0431d58620
NQPhuc <11730168+NQPhuc@users.noreply.github.com>
2022-11-26 10:30:52 +0700

    up version

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <NuxtLayout>
     <Head>
       <Link rel="shortcut icon" href="/favicon.ico" />
-      <Title>Plain Text Editor</Title>
+      <Title>Plain Text Editor v1.0.1</Title>
       <Meta name="description" content="Plain text editor is a simple web-based text editor for editing plain text" />
     </Head>
     <NuxtPage />
